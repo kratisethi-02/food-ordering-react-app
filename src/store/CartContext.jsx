6202d11e5019781005ac4e17ae3c77a4b26952a3
kratisethi-02 <kratisethi001@gmail.com>
2025-03-02 23:08:34 +0530

    Create context API for managing state

---
+++ src/store/CartContext.jsx
@@ -0,0 +1,17 @@
+import { createContext } from "react";
+
+const CartContext = createContext({
+  items: [],
+  addItem: item => {},
+  removeItem: id => {}
+});
+
+export function CartContextProvider({ children }) {
+  return (
+    <CartContext.Provider>
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+export default CartContext;
