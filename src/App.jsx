b9bbb60bff4ff9d3d5c14ae2aa4fc5bb8c9dee1f
kratisethi-02 <kratisethi001@gmail.com>
2025-03-03 13:57:39 +0530

    Created a userProgress ContextAPI to keep a track user is viewing cart or at checkout

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import Header from "./components/Header";
 import Meals from "./components/Meals";
 import { queryClient } from "./https";
 import { CartContextProvider } from "./store/CartContext";
+import { UserProgressContextProvider } from './store/UserProgressContext';
 
 function App() {
   return (
     <>
+    
      <QueryClientProvider client={queryClient}>
+      <UserProgressContextProvider>
     <CartContextProvider>
     <Header/>
     <Meals/>
     </CartContextProvider>
+    </UserProgressContextProvider>
     </QueryClientProvider>
     </>
   );
