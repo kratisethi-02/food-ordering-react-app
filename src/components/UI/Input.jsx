336adda6f4c59ecf20a9003cc91f1f9be43fef24
kratisethi-02 <kratisethi001@gmail.com>
2025-03-03 20:15:06 +0530

    Create UI component Input

---
+++ src/components/UI/Input.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Input = ({ label, id, ...props }) => {
+  return (
+    <p className="control">
+      <label htmlFor={id}>
+        {label}
+      </label>
+      <input id={id} name={id} {...props} required />
+    </p>
+  );
+};
+
+export default Input;
